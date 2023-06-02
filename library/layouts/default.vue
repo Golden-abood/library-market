10656777bcf456171f779dc4f66165d582c2e067
unknown <a.alzahabi@ejad.solutions>
2023-06-02 20:05:34 +0300

    added photo websit

---
+++ library/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1 class="text-base font-bold underline text-red-400">
+            Hello world
+        </h1>
+        <slot></slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
